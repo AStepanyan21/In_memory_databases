f188ec8f04a07a1a7ea55c0fc48db15af05f9114
AStepanyan21 <51211218+AStepanyan21@users.noreply.github.com>
2021-02-15 22:20:45 +0400

    Add server cli

---
+++ Server_cli.h
@@ -0,0 +1,40 @@
+#ifndef SERVERCLI_H
+#define SERVERCLI_H
+#include<iostream>
+#include<string>
+#include<regex>
+#include "Types.h"
+#include "Tables.h"
+#include "Server.h"
+
+class ServerCLI{
+public:
+
+
+
+private:
+    Server s;
+    void parser(std::string command){
+        std::regex ws_re("\\s+");
+        std::vector<std::string> result{
+        std::sregex_token_iterator(command.begin(), command.end(), ws_re, -1), {}
+        };
+        this->lexer(result);
+    }
+    void lexer(std::vector<std::string> token){
+
+    }
+    template<typename T>
+    T getElementByKey(T table, std::string key){
+        return this->s.getElementByKey(table, key);
+    }
+
+    template <typename T, typename R>
+    void setElementByKey(T table, std::string key, R value){
+        this->s.setElementByKey(table, key, value);
+    }
+};
+
+
+
+#endif
